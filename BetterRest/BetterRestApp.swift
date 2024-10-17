c96f4218e3b0f975c055768c0ba29458aebe1963
Razvan Pricop <razvanipricop@gmail.com>
2024-10-17 22:04:54 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Razvan Pricop on 17.10.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
